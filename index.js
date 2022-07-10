418b138c9f0936b5a004630111bf114c6433c252
Valentin <valentin.lyash@gmail.com>
2022-07-10 12:54:13 -0400

    refactoring code to ensure we remote my apiKey and instead offer the public users who want to use this api to use their own apiKey to access this data. also, inside the get routes update the api-Key to be enttered by the user and use it to generate scraper url and embed that into the response to the browser

--- index.js
+++ index.js
@@ -6,25 +6,69 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const apiKey = 'd7c73fbb4533878a7960fadb552176e3';
-const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+// const apiKey = 'd7c73fbb4533878a7960fadb552176e3';
+//require public to get their own apiKey
+// const  = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json());
 app.get('/', (req,res) =>{
     res.send('Welcome to Amazon Scraper Eureka API!')
 });
 
-//GET Product details - dynamic 
+//GET Product Details - dynamic endpoint 1
 app.get('/products/:productId', async(req,res) =>{
     const { productId } = req.params; //get productId from parameters
+    const {api_key} = req.query; //this is where we get the entered API key into the form 
     try{
-        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`)
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/dp/${productId}`)
 
         res.json(JSON.parse(response));
     } catch(error){
         res.json(error);
     }
 })
+
+//GET Product Reviews - dynamic endpoint 2
+app.get('/products/:productId/reviews', async(req,res) =>{
+    const { productId } = req.params; //get productId from parameters
+    const {api_key} = req.query; //this is where we get the entered API key into the form 
+
+    try{
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/product-reviews/${productId}`)
+
+        res.json(JSON.parse(response));
+    } catch(error){
+        res.json(error);
+    }
+})
+//GET Product Offers - dynamic endpoint 3
+app.get('/products/:productId/offers', async(req,res) =>{
+    const { productId } = req.params; //get productId from URL parameters
+    const {api_key} = req.query; //this is where we get the entered API key into the form 
+
+    try{
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
+
+        res.json(JSON.parse(response));
+    } catch(error){
+        res.json(error);
+    }
+})
+
+//GET Search Results - dynamic endpoint 4
+app.get('/search/:searchQuery', async(req,res) =>{
+    const { searchQuery } = req.params; //get searchQuery from URL parameters
+    try{
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/s?k=${searchQuery}`)
+
+        res.json(JSON.parse(response));
+    } catch(error){
+        res.json(error);
+    }
+})
+
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
 
