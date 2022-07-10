3ebcc3e17080fab18070389674cfff3c03f0202d
Valentin <valentin.lyash@gmail.com>
2022-07-10 11:30:44 -0400

    adding baseURL, and apikey from apiscraper. Creating a products/productId route to request all the data and response parsing the data in the browser

--- index.js
+++ index.js
@@ -6,9 +6,26 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = 'd7c73fbb4533878a7960fadb552176e3';
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 app.get('/', (req,res) =>{
     res.send('Welcome to Amazon Scraper Eureka API!')
 });
 
+//GET Product details - dynamic 
+app.get('/products/:productId', async(req,res) =>{
+    const { productId } = req.params; //get productId from parameters
+    try{
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`)
+
+        res.json(JSON.parse(response));
+    } catch(error){
+        res.json(error);
+    }
+})
+
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+
+
