078b7d2cc85fb6ae413a7c809cb9e51e4cf04fb3
Valentin <valentin.lyash@gmail.com>
2022-07-10 10:48:02 -0400

    initial commit. adding installed libraries in package.json as well as begining of server code in index.js using express routes and request-promise library (request)

---
+++ index.js
@@ -0,0 +1,14 @@
+//This is out API 
+
+const express = require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.get('/', (req,res) =>{
+    res.send('Welcome to Amazon Scraper Eureka API!')
+});
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
